c3d92cf92ce81b4458b7fb1ea0950531ee88cbfe
Hans-Christoph Steiner <hans@eds.org>
2016-10-30 22:30:37 +0100

    Imported Upstream version 1.1

---
+++ src/main/java/de/onyxbits/dummydroid/DummyDroidProperties.java
@@ -0,0 +1,20 @@
+package de.onyxbits.dummydroid;
+
+/**
+ * Inofficial build properties
+ * 
+ * @author patrick
+ * 
+ */
+public interface DummyDroidProperties {
+
+	public static final String SCREENWIDTH = "dd.screenwidth";
+	public static final String SCREENHEIGHT = "dd.screenheight";
+	public static final String LOCALE = "dd.locale";
+	public static final String TIMEZONE = "dd.timezone";
+	public static final String CELLOPERATOR = "dd.celloperator";
+	public static final String SIMOPERATOR = "dd.simoperator";
+	public static final String FEATURES = "dd.features";
+	public static final String LIBRARIES = "dd.libraries";
+	public static final String PLATFORMS = "dd.native";
+}
